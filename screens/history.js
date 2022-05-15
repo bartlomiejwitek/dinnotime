4c41ff1d685f32f1a9e0a092e222fba12e3ab1c0
bartlomiejwitek <bartlomiej.witek98@gmail.com>
2022-05-15 21:14:41 +0200

    added animations

---
+++ screens/history.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function History() {
+  return (
+    <SafeAreaView>
+        <Text>Hello history</Text>
+    </SafeAreaView>
+  );
+}
